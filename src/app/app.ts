9a1c9061d27c90d96975555e7ef4ef9217d204ac
lukoq <kinderlukasz01@gmail.com>
2026-01-29 13:24:39 +0100

    Add a dark mode

--- src/app/app.ts
+++ src/app/app.ts
@@ -12,4 +12,8 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('task-manager-frontend');
+
+  toggleTheme(): void {
+    document.body.classList.toggle('dark-theme');
+  }
 }
