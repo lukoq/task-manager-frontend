7507d783f440de62c0286cef0aad89773e72331a
lukoq <kinderlukasz01@gmail.com>
2026-02-09 13:57:41 +0100

    Add end-of-session support

--- src/app/auth/token/jwt-interceptor.ts
+++ src/app/auth/token/jwt-interceptor.ts
@@ -1,22 +1,38 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import { HttpInterceptorFn, HttpErrorResponse } from '@angular/common/http';
 import { inject, PLATFORM_ID } from '@angular/core';
 import { isPlatformBrowser } from '@angular/common';
-
+import { catchError, throwError } from 'rxjs';
+import { Router } from '@angular/router';
 
 export const jwtInterceptor: HttpInterceptorFn = (req, next) => {
-
   const platformId = inject(PLATFORM_ID);
+  const router = inject(Router);
+
+  let authReq = req;
+
   if(isPlatformBrowser(platformId)) {
     const token = localStorage.getItem('token');
-
     if(token) {
-      const authReq = req.clone({
+      authReq = req.clone({
         setHeaders: {
           Authorization: `Bearer ${token}`
         }
       });
-      return next(authReq);
     }
   }
-  return next(req);
+
+  return next(authReq).pipe(
+    catchError((error: HttpErrorResponse) => {
+      if (error.status === 401 || error.status === 403) {
+        console.error('Token has expired');
+        
+        if (isPlatformBrowser(platformId)) {
+          localStorage.removeItem('token'); 
+        }
+        
+        router.navigate(['/login']); 
+      }
+      return throwError(() => error);
+    })
+  );
 };
