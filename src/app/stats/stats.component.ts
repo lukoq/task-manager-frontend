aac058d2059eff22aff6669446e6e681aee76e39
lukoq <kinderlukasz01@gmail.com>
2026-02-16 22:33:19 +0100

    Add stats.component

---
+++ src/app/stats/stats.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  templateUrl: './stats.component.html',
+  imports: [ ],
+  styleUrls: ['./stats.component.css']
+})
+export class StatsComponent {
+  
+    
+}
