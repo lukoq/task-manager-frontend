983c60206a62450f9408a6c41e9c725e95f62fe4
lukoq <kinderlukasz01@gmail.com>
2026-03-02 12:02:35 +0100

    Add a new password form

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,14 +1,49 @@
 import { Component, computed, inject, signal } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   templateUrl: './profile.component.html',
-  imports: [],
+  imports: [ReactiveFormsModule],
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent {
-  
+  isChangeModalOpen = signal(false);
 
+  changePasswordForm = new FormGroup({
+    oldPassword: new FormControl('', { 
+      nonNullable: true, 
+      validators: [Validators.required] 
+    }),
+    newPassword: new FormControl('', { 
+      nonNullable: true,
+      validators: [Validators.required, Validators.minLength(5)] 
+    }),
+    repeatNewPassword: new FormControl('', { 
+      nonNullable: true,
+      validators: [Validators.required, Validators.minLength(5)] 
+    }),
+  });
+
+
+  openChangeModal() {
+    this.isChangeModalOpen.set(true);
+  }
+
+  closeChangeModal() {
+    this.isChangeModalOpen.set(false);
+    this.changePasswordForm.reset(); 
+  }
+
+  onSubmit() {
+    if(this.changePasswordForm.valid) {
+      const formData = this.changePasswordForm.getRawValue();
+      this.closeChangeModal(); 
+    } 
+    else {
+      this.changePasswordForm.markAllAsTouched();
+    }
+  }
 
 }
